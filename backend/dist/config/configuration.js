a1aeef1d73ba7a3f93112b5d989e5e955482c363
veerendra thogiti <thogitiveerendra5@gmail.com>
2025-07-05 10:02:22 +0530

    Update configuration.js

--- backend/dist/config/configuration.js
+++ backend/dist/config/configuration.js
@@ -7,7 +7,7 @@ exports.default = () => {
             host: process.env.DATABASE_HOST || 'localhost',
             port: parseInt(process.env.DATABASE_PORT || '5432', 10),
             username: process.env.DATABASE_USERNAME || 'postgres',
-            password: process.env.DATABASE_PASSWORD || 'Veerendra21@',
+            password: process.env.DATABASE_PASSWORD || '',
             database: process.env.DATABASE_NAME || 'nest',
         },
         jwt: {
